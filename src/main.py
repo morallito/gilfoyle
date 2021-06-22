660bb5f4d0b5c5a12c254b00efc40533c6f9f2ed
Iago Henrique Pires Moreira e Morais <iagohenrique20@gmail.com>
2021-06-21 21:08:30 -0300

    Scaffold Gilfoyle (#1)
    * Add fancy description in README.md
    
    * Create LICENSE.md
    
    * update readme
    
    * add project architecture
    
    * add git lfs
    
    * add venv to git ignore
    
    * configure venv creation / initialization created in makefile
    
    * add editorconfig
    
    * add gitignore file for python
    
    * Scaffold python application
    
    * Add black, formate code

---
+++ src/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from messenger.messenger import console_log_string
+from messenger import *
+from logstrings import *
+
+
+console_log_string("LALA")
---
+++ src/messenger/messenger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from logstrings import MESSAGE_LOGGED_SUCCESSFULLY, MESSAGE_LOGGING_ERROR
+from returns.result import Result, Failure, Success
+from logstrings import *
+
+
+def console_log_string(message: str) -> Result[Failure, Success]:
+    try:
+        print(message)
+        return Result(Success(MESSAGE_LOGGED_SUCCESSFULLY))
+    except Exception as error:
+        return Result(Failure(MESSAGE_LOGGING_ERROR.format(error, message)))
